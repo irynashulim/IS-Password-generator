64ba97d24b0bac8675c8da9d5cdb9c3a8f232f43
Iryna <iryna.shulim@gmail.com>
2021-05-21 17:30:55 -0500

    writePassword() function and generatePassword() function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
 
+var randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
+
 //generator functions
 //using https://www.w3schools.com/html/html_charset.asp
 
@@ -21,20 +29,27 @@ function getRandomSymbol() {
      return symbols[Math.floor(Math.random() * symbols.length)];
     }
   
-  
-    console.log(getRandomSymbol());
-   
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+    var length = +lengthEl.value;
+    var hasLower = lowercaseEl.confirm;
+    var hasUpper = uppercaseEl.confirm;
+    var hasNumber = numberEl.confirm;
+    var hasSymbol = symbolEl.confirm;
+  var password = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+function generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol) {
+    var generatedPassword = '';
+    var typesCount = lower + upper + number + symbol;
+    var typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
