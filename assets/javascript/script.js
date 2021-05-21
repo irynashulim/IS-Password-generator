4dcf457ecd9c589c3b83fc3d0833f255d851a14b
Iryna <iryna.shulim@gmail.com>
2021-05-21 13:38:29 -0500

    generator functions added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,29 @@
 // Assignment code here
 
+//generator functions
+//using https://www.w3schools.com/html/html_charset.asp
+
+//Lower Case function
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+   }
+//Upper case function
+ function getRandomUpper() {
+     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    }
+// Numbers function
+function getRandomNumber() {
+     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    }
+//Symbols function
+function getRandomSymbol() {
+      const symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+     return symbols[Math.floor(Math.random() * symbols.length)];
+    }
+  
+  
+    console.log(getRandomSymbol());
+   
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
