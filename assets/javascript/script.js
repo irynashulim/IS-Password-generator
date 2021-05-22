6d89a378d76767b3e233c26c645b60295430b59d
Iryna <iryna.shulim@gmail.com>
2021-05-22 10:11:11 -0500

    functions update and add variables

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,33 +1,37 @@
 // Assignment code here
 
-var randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomSymbol
-};
-
-
+var passwordLength;
+var hasLower;
+var hasUpper;
+var hasNumber;
+var hasSymbol;
+var usedChoice;
+ 
 //generator functions
 //using https://www.w3schools.com/html/html_charset.asp
 
 //Lower Case function
-function getRandomLower() {
+var getRandomLower = function() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-   }
+   };
+   console.log(getRandomLower());
+
 //Upper case function
- function getRandomUpper() {
+var getRandomUpper = function() {
      return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-    }
+    };
+    console.log(getRandomUpper());
 // Numbers function
-function getRandomNumber() {
+var getRandomNumber = function() {
      return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-    }
+    };
+    console.log(getRandomNumber());
 //Symbols function
-function getRandomSymbol() {
+var getRandomSymbol = function() {
       const symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
      return symbols[Math.floor(Math.random() * symbols.length)];
-    }
+    };
+    console.log(getRandomSymbol());
   
 
 // Get references to the #generate element
@@ -35,21 +39,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var length = +lengthEl.value;
-    var hasLower = lowercaseEl.confirm;
-    var hasUpper = uppercaseEl.confirm;
-    var hasNumber = numberEl.confirm;
-    var hasSymbol = symbolEl.confirm;
-  var password = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
+ 
+  var password = generatePassword()
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
-function generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol) {
-    var generatedPassword = '';
-    var typesCount = lower + upper + number + symbol;
-    var typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+function generatePassword() {
+    passwordLength = prompt("Please enter how many characters for your password? btwn 8-128");
+    if (!passwordLength) {
+    alert("Needs value");
+} else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Needs to be between 8 and 128 characters.");
+}
+hasLower = confirm("Do you wanna use lower case?") 
+hasUpper = confirm("Do you wanna use upper case?")
+hasNumber = confirm("Do you wanna use numbers?")
+hasSymbol = confirm("Do you wanna use symbols?")
+    
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
